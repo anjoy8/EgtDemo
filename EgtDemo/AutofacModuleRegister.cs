9f3cc3c4935b0c462be9cf61b2406730d828cc81
anjoy8 <3143422472@qq.com>
2020-06-01 06:41:43 +0800

    master : remove aop

--- EgtDemo/AutofacModuleRegister.cs
+++ EgtDemo/AutofacModuleRegister.cs
@@ -1,9 +1,5 @@
 ﻿using Autofac;
-using Autofac.Core;
-using Autofac.Extras.DynamicProxy;
-using EgtDemo.Extensions.Aop;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -15,11 +11,6 @@ namespace EgtDemo
         {
             var basePath = AppContext.BaseDirectory;
             //builder.RegisterType<AdvertisementServices>().As<IAdvertisementServices>();
-
-            var cacheType = new List<Type>();
-
-            builder.RegisterType<BlogLogAOP>();
-            cacheType.Add(typeof(BlogLogAOP));
 
 
             #region 带有接口层的服务注入
@@ -58,13 +49,6 @@ namespace EgtDemo
                    .InstancePerDependency();
 
             #endregion
-
-
-            //只能注入该类中的虚方法，且必须是public
-            //这里仅仅是一个单独类无接口测试，不用过多追问
-            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(LoveU)))
-                .EnableClassInterceptors()
-                .InterceptedBy(cacheType.ToArray());
 
 
         }
