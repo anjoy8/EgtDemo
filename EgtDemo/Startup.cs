5a3d811476c8f800cee926ea4f78bc9f89c7d9d0
anjoy8 <3143422472@qq.com>
2020-06-30 11:24:08 +0800

    update

--- EgtDemo/Startup.cs
+++ EgtDemo/Startup.cs
@@ -1,29 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using BCVP.Sample.Common;
-using BCVP.Sample.OPConsole.Extensions;
+using BCVP.Sample.Extensions;
 using EgtDemo.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace EgtDemo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,6 +28,7 @@ namespace EgtDemo
 
             services.AddControllers();
 
+            services.AddBCVPServiceInit(Configuration,Env);
             services.AddBCVPSqlsugarExtensions();
 
             services.AddAuthorizationSetup();
